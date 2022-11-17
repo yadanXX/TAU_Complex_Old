fc01a68972b13c0e4ea5edc734b33d7636c74474
Danil <dan.ya-dan@yandex.ru>
2022-11-17 13:24:47 +1000

    Неверная переходная характиристика запаздывания

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -28,19 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panelmain = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl3 = new ZedGraph.ZedGraphControl();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.textBoxT = new System.Windows.Forms.TextBox();
+            this.textBoxtau = new System.Windows.Forms.TextBox();
+            this.textBoxw = new System.Windows.Forms.TextBox();
+            this.textBoxtk = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelmain.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelmain
+            // 
+            this.panelmain.Controls.Add(this.panel3);
+            this.panelmain.Controls.Add(this.panel2);
+            this.panelmain.Controls.Add(this.panel1);
+            this.panelmain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelmain.Location = new System.Drawing.Point(0, 0);
+            this.panelmain.Name = "panelmain";
+            this.panelmain.Size = new System.Drawing.Size(917, 623);
+            this.panelmain.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBoxtk);
+            this.panel1.Controls.Add(this.textBoxw);
+            this.panel1.Controls.Add(this.textBoxtau);
+            this.panel1.Controls.Add(this.textBoxT);
+            this.panel1.Controls.Add(this.textBoxK);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(451, 623);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.zedGraphControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(451, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(466, 308);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.zedGraphControl3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(451, 314);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(466, 309);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.zedGraphControl2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 349);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(451, 274);
+            this.panel4.TabIndex = 1;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zedGraphControl1.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(466, 308);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zedGraphControl2.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(451, 274);
+            this.zedGraphControl2.TabIndex = 1;
+            this.zedGraphControl2.UseExtendedPrintDialog = true;
+            // 
+            // zedGraphControl3
+            // 
+            this.zedGraphControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zedGraphControl3.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl3.Name = "zedGraphControl3";
+            this.zedGraphControl3.ScrollGrace = 0D;
+            this.zedGraphControl3.ScrollMaxX = 0D;
+            this.zedGraphControl3.ScrollMaxY = 0D;
+            this.zedGraphControl3.ScrollMaxY2 = 0D;
+            this.zedGraphControl3.ScrollMinX = 0D;
+            this.zedGraphControl3.ScrollMinY = 0D;
+            this.zedGraphControl3.ScrollMinY2 = 0D;
+            this.zedGraphControl3.Size = new System.Drawing.Size(466, 309);
+            this.zedGraphControl3.TabIndex = 1;
+            this.zedGraphControl3.UseExtendedPrintDialog = true;
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(150, 98);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(100, 20);
+            this.textBoxK.TabIndex = 2;
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(150, 124);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(100, 20);
+            this.textBoxT.TabIndex = 4;
+            // 
+            // textBoxtau
+            // 
+            this.textBoxtau.Location = new System.Drawing.Point(150, 150);
+            this.textBoxtau.Name = "textBoxtau";
+            this.textBoxtau.Size = new System.Drawing.Size(100, 20);
+            this.textBoxtau.TabIndex = 6;
+            // 
+            // textBoxw
+            // 
+            this.textBoxw.Location = new System.Drawing.Point(150, 176);
+            this.textBoxw.Name = "textBoxw";
+            this.textBoxw.Size = new System.Drawing.Size(100, 20);
+            this.textBoxw.TabIndex = 8;
+            // 
+            // textBoxtk
+            // 
+            this.textBoxtk.Location = new System.Drawing.Point(150, 202);
+            this.textBoxtk.Name = "textBoxtk";
+            this.textBoxtk.Size = new System.Drawing.Size(100, 20);
+            this.textBoxtk.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(896, 572);
+            this.ClientSize = new System.Drawing.Size(917, 623);
+            this.Controls.Add(this.panelmain);
             this.Name = "Form4";
             this.Text = "Form4";
+            this.Resize += new System.EventHandler(this.Form4_Resize);
+            this.panelmain.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelmain;
+        private System.Windows.Forms.Panel panel4;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private System.Windows.Forms.Panel panel3;
+        private ZedGraph.ZedGraphControl zedGraphControl3;
+        private System.Windows.Forms.Panel panel2;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxtk;
+        private System.Windows.Forms.TextBox textBoxw;
+        private System.Windows.Forms.TextBox textBoxtau;
+        private System.Windows.Forms.TextBox textBoxT;
+        private System.Windows.Forms.TextBox textBoxK;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using org.mariuszgromada.math.mxparser;
+using ZedGraph;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
@@ -20,15 +21,116 @@ namespace TAU_Complex
             InitializeComponent();
         }
 
-        public double f(double x)
+
+        private void Form4_Resize(object sender, EventArgs e)
         {
-            string input = ("вход"); // строка которую нужно разобрать
-            org.mariuszgromada.math.mxparser.Function f = new org.mariuszgromada.math.mxparser.Function(input); // создаем функцию
+            panel1.Height = panelmain.Height / 2;
+            panel1.Width = panelmain.Width / 2;
+            panel2.Height = panelmain.Height / 2;
+            panel2.Width = panelmain.Width / 2;
+            panel3.Height = panelmain.Height / 2;
+            panel3.Width = panelmain.Width / 2;
+            panel4.Height = panelmain.Height / 2;
+            panel4.Width = panelmain.Width / 2;
+        }
 
-            double result;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double k = Convert.ToDouble(textBoxK.Text);
+            double T = Convert.ToDouble(textBoxT.Text);
+            double tk = Convert.ToDouble(textBoxtk.Text);
+            double tau = Convert.ToDouble(textBoxtau.Text);
+            double w = Convert.ToDouble(textBoxw.Text);
 
-            return result = f.calculate(x);
+            PointPairList list_1 = new PointPairList();
+            double Dt = 0.001;
+            double XInt = 0, Xe = 0;
+            double x1I = 0, x2I = 0;
+            double xv = 1;
+            for (double i = 0; i < tk; i += Dt)
+            {
+                (XInt, x1I, x2I) = Integrating(xv - Xe, k, T, x1I, x2I, Dt);
+                Xe = XInt * (i - tau);
+                list_1.Add(i, Xe);
+            }
+            DrawGraph(zedGraphControl1, list_1, "График переходной характиристики", "h(t)", "t");
+        }
 
+        private (double, double, double) Integrating(double xv, double k, double T, double x1, double x2, double Dt)
+        {
+            /* xv - вход
+             * k - коэф усиления
+             * T - постоянная времени
+             * x1 - предыдущий выход1(промежуточный), после изменения текущий1
+             * x2 - предыдущий выход2(промежуточный), после изменения текущий2 x'
+             * Dt - дельта t
+             * x3 - выход1'' (промежуточный производный)
+             * x - выход
+             */
+            double x, x3;
+            x3 = xv - x2;
+            x2 = x2 + (x3 * Dt) / T;
+            x1 = x1 + x2 * Dt;
+            x = k * x1;
+            return (x, x1, x2);
+        }
+        private void DrawGraph(ZedGraphControl zedGraphControl, PointPairList list_1, string TitleText, string YText, string XText)
+        {
+            /*
+             * zedGraphControl - рабоячая область Zграфика
+             * list_1 - массив точек x и y на графике
+             * TitleText - заголовок графика
+             * YText , XText - подписи осей
+             * Результат - постоение графика на выйбраной рабочей области с входными данными и нанесением сетки
+             */
+            GraphPane pane = zedGraphControl.GraphPane;
+            pane.CurveList.Clear();
+            LineItem myCurve1 = pane.AddCurve("", list_1, Color.Red, SymbolType.None);
+            pane.Title.Text = TitleText;
+            pane.YAxis.Title.Text = YText;
+            pane.XAxis.Title.Text = XText;
+            // !!!
+            // Включаем отображение сетки напротив крупных рисок по оси X
+            pane.XAxis.MajorGrid.IsVisible = true;
+
+            // Задаем вид пунктирной линии для крупных рисок по оси X:
+            // Длина штрихов равна 10 пикселям, ...
+            pane.XAxis.MajorGrid.DashOn = 10;
+
+            // затем 5 пикселей - пропуск
+            pane.XAxis.MajorGrid.DashOff = 5;
+
+
+            // Включаем отображение сетки напротив крупных рисок по оси Y
+            pane.YAxis.MajorGrid.IsVisible = true;
+
+            // Аналогично задаем вид пунктирной линии для крупных рисок по оси Y
+            pane.YAxis.MajorGrid.DashOn = 10;
+            pane.YAxis.MajorGrid.DashOff = 5;
+
+
+            // Включаем отображение сетки напротив мелких рисок по оси X
+            pane.YAxis.MinorGrid.IsVisible = true;
+
+            // Задаем вид пунктирной линии для крупных рисок по оси Y:
+            // Длина штрихов равна одному пикселю, ...
+            pane.YAxis.MinorGrid.DashOn = 1;
+
+            // затем 2 пикселя - пропуск
+            pane.YAxis.MinorGrid.DashOff = 2;
+
+            // Включаем отображение сетки напротив мелких рисок по оси Y
+            pane.XAxis.MinorGrid.IsVisible = true;
+
+            // Аналогично задаем вид пунктирной линии для крупных рисок по оси Y
+            pane.XAxis.MinorGrid.DashOn = 1;
+            pane.XAxis.MinorGrid.DashOff = 2;
+            myCurve1.Line.IsVisible = true;
+            pane.YAxis.Scale.MinAuto = true;
+            pane.YAxis.Scale.MaxAuto = true;
+            pane.AxisChange();
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
         }
     }
 }
